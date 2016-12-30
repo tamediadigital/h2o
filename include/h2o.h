78d7add19b080f4139f2fcf1042d74be53ebd7b7
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-12-30 02:08:48 +0200

    fix CMAKE config

--- include/h2o.h
+++ include/h2o.h
@@ -50,7 +50,10 @@ extern "C" {
 #include "h2o/timeout.h"
 #include "h2o/url.h"
 #include "h2o/version.h"
+
+#ifdef WITH_RDKAFKA
 #include "rdkafka.h"
+#endif
 
 #ifndef H2O_USE_BROTLI
 /* disabled for all but the standalone server, since the encoder is written in C++ */
@@ -1541,6 +1544,7 @@ h2o_logger_t *h2o_access_log_register(h2o_pathconf_t *pathconf, h2o_access_log_f
 void h2o_access_log_register_configurator(h2o_globalconf_t *conf);
 
 /* lib/kafka_log.c */
+#ifdef WITH_RDKAFKA
 
 typedef struct st_h2o_kafka_log_handle_t h2o_kafka_log_handle_t;
 
@@ -1554,6 +1558,8 @@ h2o_kafka_log_handle_t *h2o_kafka_log_open_handle(rd_kafka_conf_t* rk_conf,
     const char *fmt_hash);
 h2o_logger_t *h2o_kafka_log_register(h2o_pathconf_t *pathconf, h2o_kafka_log_handle_t *handle);
 void h2o_kafka_log_register_configurator(h2o_globalconf_t *conf);
+
+#endif
 
 /* lib/chunked.c */
 
